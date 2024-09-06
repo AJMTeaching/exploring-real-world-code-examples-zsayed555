b77dd1d76f6a0fbc030b0bffa2b5e0d0cf61220d
zsayed555 <zsayed1@luc.edu>
2024-09-06 06:25:47 +0000

    Exploring Code in Daily Life

---
+++ .github/example.py
@@ -0,0 +1,5 @@
+# if I want to check a number or any other value is in a list, I can use the key word 'in'
+print ("Example of using in to determine if something is a member of a data structure")
+value = 2
+print(if p) [{
+alarm clock 
